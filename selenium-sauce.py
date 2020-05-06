dbefe2c8321247c9ae9b6efb7cc6aee69f4e9455
Nati-Pi <natespring92@gmail.com>
2020-05-06 07:19:35 -0600

    selenium changes for headless

--- selenium-sauce.py
+++ selenium-sauce.py
@@ -3,7 +3,8 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-browser = webdriver.Chrome(options=chrome_options)
+#'/usr/lib/chromium-browser/chromedriver'
+browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=chrome_options)
 browser.get('https://www.google.com')
 html = browser.page_source
 print(html[:100])
