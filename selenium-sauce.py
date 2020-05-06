d89059f9e78f01ce97bed3bdf0832d48efc07ada
Nate <natespring92@gmail.com>
2020-05-05 21:53:34 -0600

    First commit to Pi

---
+++ selenium-sauce.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+browser.get('https://www.google.com')
+html = browser.page_source
+print(html[:100])
+browser.quit()
