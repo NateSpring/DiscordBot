773047523fca14f972c572f60c32d671081cd30a
Nate <natespring92@gmail.com>
2020-05-06 07:21:50 -0600

    new selenium sauce changes

--- selenium-sauce.py
+++ selenium-sauce.py
@@ -4,11 +4,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 #'/usr/lib/chromium-browser/chromedriver'
-<<<<<<< HEAD
-browser = webdriver.Chrome(options=chrome_options)
-=======
 browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=chrome_options)
->>>>>>> dbefe2c8321247c9ae9b6efb7cc6aee69f4e9455
 browser.get('https://www.google.com')
 html = browser.page_source
 print(html[:100])
