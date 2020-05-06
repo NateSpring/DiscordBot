aa4e0bf179d14b2d81e424b82fdd6c2908f0cddc
Nate <natespring92@gmail.com>
2020-05-06 07:40:59 -0600

    butterbot addition

---
+++ butter-robot/butter-robot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from simplesms import sendtxt
+
+TOKEN = 'NzA2ODUyNDQ4ODMyMzIzNjc0.XrATEA.vsrZkWGMt6woR-w4imFpuj72qLQ'
+
+description = '''My purpose is to pass the butter'''
+bot = commands.Bot(command_prefix='!', description=description)
+
+@bot.event
+async def on_ready():
+    print('Butter Robot Online')
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def hello(ctx):
+    """I will welcome your presence"""
+    user = ctx.message.author
+    await ctx.send("Hello " + user.mention + "!")
+
+@bot.command()
+async def purpose(ctx):
+    """I'll explain my purpose"""
+    await ctx.send("To pass the butter.")
+
+@bot.command()
+async def text(ctx, *text):
+    """I'll relay your message to my master"""
+    await ctx.send("Your message shall be relayed. Thank You.")
+    sendtxt(" ".join(text[:]))
+
+bot.run(TOKEN)
--- selenium-sauce.py
+++ selenium-sauce.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 #'/usr/lib/chromium-browser/chromedriver'
-browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=chrome_options)
+browser = webdriver.Chrome(options=chrome_options)
 browser.get('https://www.google.com')
 html = browser.page_source
 print(html[:100])
