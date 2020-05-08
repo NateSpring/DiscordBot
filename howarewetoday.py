d51f12f4043aae786632faebe8b19306dab836d2
Nate <natespring92@gmail.com>
2020-05-08 17:35:47 -0600

    pulled changes for sentiment analysis

--- howarewetoday.py
+++ howarewetoday.py
@@ -97,7 +97,7 @@ def main():
     # pcking positive tweets from tweets 
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
     # percentage of positive tweets 
-    print(queryArray[0].text)
+    print(str(queryArray)[1:-1])
     print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
     # picking negative tweets from tweets 
     ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
