d63dd40d4cd888b48d797305663aae58b4154a5d
Nate <natespring92@gmail.com>
2020-05-07 22:09:52 -0600

    first real pi push

--- howarewetoday.py
+++ howarewetoday.py
@@ -33,7 +33,7 @@ class TwitterClient(object):
         Utility function to clean tweet text by removing links, special characters 
         using simple regex statements. 
         '''
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split()) 
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split()) 
   
     def get_tweet_sentiment(self, tweet): 
         ''' 
@@ -101,7 +101,7 @@ def main():
     # percentage of negative tweets 
     print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
     # percentage of neutral tweets 
-    print("Neutral tweets percentage: {} % \ ".format(100*len(tweets - ntweets - ptweets)/len(tweets))
+    print("Neutral tweets percentage: {} %".format(100*len(tweets - ntweets - ptweets)/len(tweets))) 
   
     # printing first 5 positive tweets 
     print("\n\nPositive tweets:") 
