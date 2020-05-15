5714c7d8190354395cbb040b654f1688528bec67
Nate <natespring92@gmail.com>
2020-05-14 22:45:52 -0600

    Butter bot has ears

--- butter-robot/butter-robot.py
+++ butter-robot/butter-robot.py
@@ -41,6 +41,7 @@ async def text(ctx, *text):
 @bot.command()
 async def feels(ctx, *text):
     """I'll perform sentiment analysis on the given criteria based on Tweets"""
-    await ctx.send(main(" ".join(text[:])))
+    print(" ".join(text[:]))
+    #await ctx.send(main(" ".join(text[:])))
 
 bot.run(TOKEN)
