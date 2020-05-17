bd49fcb565b7393790c5f6c7d3d16e54627db1b8
Nate <natespring92@gmail.com>
2020-05-16 22:25:51 -0600

    commiting the PRAW

--- butter-bot/butter-bot.py
+++ butter-bot/butter-bot.py
@@ -58,7 +58,7 @@ async def meme(ctx):
     for i in range(0, post_to_pick):
         submission = next(x for x in memes_submissions if not x.stickied)
 
-    await bot.say(submission.url)
+    await ctx.message.channel(submission.url)
 
 
 
