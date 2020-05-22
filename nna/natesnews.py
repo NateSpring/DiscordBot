351fbe28af209d005e1a80e8e5f9f47a06e0d327
Nate <natespring92@gmail.com>
2020-05-22 09:07:00 -0600

    send changes to pi for nna

--- nna/natesnews.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-r = requests.get('https://www.indiehackers.com/products?sorting=highest-revenue')
-soup = BeautifulSoup(r.content, 'html5lib')
-
-headings = soup.find_all('article', {'class': 'Card-sc-4llv5q-0 fkexff'})
-for heading in headings:
-    print(heading.a.text)
