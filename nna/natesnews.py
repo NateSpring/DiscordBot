000c98ed9be71c4a98a8892a223e454ef6eddc3f
Nate <natespring92@gmail.com>
2020-05-21 22:38:44 -0600

    Adding NNA

--- nna/natesnews.py
+++ nna/natesnews.py
@@ -5,3 +5,5 @@ r = requests.get('https://www.indiehackers.com/products?sorting=highest-revenue'
 soup = BeautifulSoup(r.content, 'html5lib')
 
 headings = soup.find_all('article', {'class': 'Card-sc-4llv5q-0 fkexff'})
+for heading in headings:
+    print(heading.a.text)
