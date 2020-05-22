b3f4007fcb0a8df5cf954956f3df3da9c606b9b9
Nate <natespring92@gmail.com>
2020-05-22 09:15:34 -0600

    send changes to pi for nna

--- gitcommit-site/gitcommit-site.py
+++ gitcommit-site/gitcommit-site.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-
+data = pd.read_csv('gitlog430.csv')
+data.columns = ["Author", "Day", "Date", "Message", " ", " "]
+df = data[['Day', 'Date', 'Message']]
 
 
 app = Flask(__name__)
