ed12d3478b65ed925a530a43fbbec96b5420bce0
Nate <natespring92@gmail.com>
2020-05-22 08:15:55 -0600

    send changes to pi for nna

--- nna/testsite.py
+++ nna/testsite.py
@@ -15,4 +15,4 @@ def index():
     return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
