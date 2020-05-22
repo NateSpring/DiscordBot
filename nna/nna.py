e267381e58201df10c2716f504cb5895d0fc4c43
Nate <natespring92@gmail.com>
2020-05-22 09:27:36 -0600

    send changes to pi for nna

--- nna/nna.py
+++ nna/nna.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
