0970f90f6c6ff35412e26c128b8e6c1635e51397
Nate <natespring92@gmail.com>
2020-05-23 21:27:26 -0600

    Pushing new css styles for NNA

--- nna/nna.py
+++ nna/nna.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
 
 
-
+url = 'https://www.cnet.com/news/'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+headlines = soup.find_all('div', class_='col-2 assetText')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
