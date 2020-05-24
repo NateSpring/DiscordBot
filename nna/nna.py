7666859c0df60096eb6a2276310fb71568c9e395
Nate <natespring92@gmail.com>
2020-05-23 21:35:35 -0600

    Pushing new css styles for NNA

--- nna/nna.py
+++ nna/nna.py
@@ -6,10 +6,10 @@ import requests
 url = 'https://www.cnet.com/news/'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
-headlines = soup.find_all('div', class_='col-2 assetText').text
+headlines = soup.find_all('div', class_='col-2 assetText')
 news = []
 for headline in headlines:
-    news.append(headline)
+    news.append(headline.text)
 
 app = Flask(__name__)
 
