fbfa9c8eeb63840cdc648c339089451add4df5a8
Nate <natespring92@gmail.com>
2020-05-23 22:52:47 -0600

    Pushing new css styles for NNA

--- nna/nna.py
+++ nna/nna.py
@@ -7,6 +7,8 @@ url = 'https://www.dev.to'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 headlines = soup.find_all('div', class_='crayons-story__body')
+link = headlines.a.href
+
 
 
 
@@ -14,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', news=headlines)
+    return render_template('index.html', news=headlines, link=link)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
