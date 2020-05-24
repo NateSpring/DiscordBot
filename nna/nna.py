8516d0c6148eb78912985f2581d46d377411efab
Nate <natespring92@gmail.com>
2020-05-23 23:01:07 -0600

    Pushing new css styles for NNA

--- nna/nna.py
+++ nna/nna.py
@@ -6,8 +6,8 @@ import requests
 url = 'https://www.dev.to'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
-headlines = soup.find_all('div', class_='crayons-story__body')
-link = headlines.find("a")
+headlines = soup.find('div', class_='crayons-story__body')
+link = headlines.find_all("a")
 
 
 
