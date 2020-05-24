796663e7f892abcbda2681e792f4faec421ee160
Nate <natespring92@gmail.com>
2020-05-23 21:58:29 -0600

    Pushing new css styles for NNA

--- nna/nna.py
+++ nna/nna.py
@@ -9,7 +9,7 @@ soup = BeautifulSoup(r.text, "html.parser")
 headlines = soup.find_all('div', class_='col-2 assetText')
 news = {}
 for headline in headlines:
-    news[header].append(headline.text)
+    news[header] = news.append(headline.text)
 
 app = Flask(__name__)
 
