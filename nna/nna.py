2062427e8918435d2555d47f20da2d8855c8efe1
Nate <natespring92@gmail.com>
2020-05-23 22:26:33 -0600

    Pushing new css styles for NNA

--- nna/nna.py
+++ nna/nna.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', headlines=news)
+    return render_template('index.html', news=healines)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
