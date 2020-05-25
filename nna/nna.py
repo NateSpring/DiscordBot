1b108bc08a7413013582bee351b5e6a5ef0b2098
Nate <natespring92@gmail.com>
2020-05-25 07:18:45 -0600

    some commitment, singular link

--- nna/nna.py
+++ nna/nna.py
@@ -10,7 +10,7 @@ soup = BeautifulSoup(r.text, "html.parser")
 title = soup.find_all('h2', class_='crayons-story__title')
 
 headlines = soup.find_all('div', class_='crayons-story__indention')
-link = headline.findChild('h2', class_='crayons-story__title').a['href']
+link = headlines.findChild('h2', class_='crayons-story__title').a['href']
 
 app = Flask(__name__)
 
