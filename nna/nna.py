a9420fd696ef2330543d202e35524f4ed22a7c29
Nati-Pi <natespring92@gmail.com>
2020-05-24 21:24:05 -0600

    yes

--- nna/nna.py
+++ nna/nna.py
@@ -6,8 +6,8 @@ import requests
 url = 'https://www.dev.to'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
-headlines = soup.find('div', class_='crayons-story__body')
-link = headlines.find_all("a")
+headlines = soup.find_all('div', class_='crayons-story__body')
+print(soup)
 
 
 
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', news=headlines, link=link)
+    return render_template('index.html', news=headlines)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
