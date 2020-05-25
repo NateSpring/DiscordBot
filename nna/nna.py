57c245faa2b9591f4b6c6b43369dc91a34c8858a
Nate <natespring92@gmail.com>
2020-05-24 21:34:02 -0600

    new changes

--- nna/nna.py
+++ nna/nna.py
@@ -7,7 +7,7 @@ url = 'https://www.dev.to'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 
-headlines = soup.findAll('div', class_='crayons-story__body')
+headlines = soup.findAll('div', class_='crayons-story__indention')
 
 for headline in headlines:
     link = headline
