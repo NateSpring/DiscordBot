5217e2a697c35db38ca127dc8e4a787ed1e32a73
Nate <natespring92@gmail.com>
2020-05-24 21:23:29 -0600

    pushing changes yo

--- nna/nna.py
+++ nna/nna.py
@@ -6,16 +6,17 @@ import requests
 url = 'https://www.dev.to'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
-headlines = soup.find('div', class_='crayons-story__body')
-link = headlines.find_all("a")
-
+headlines = soup.find('div', class_='crayons-story__title')
+link = headlines.find('a')['href']
+for allLinks in link:
+    print(link)
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', news=headlines, link=link)
+    return render_template('index.html', news=headlines)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
